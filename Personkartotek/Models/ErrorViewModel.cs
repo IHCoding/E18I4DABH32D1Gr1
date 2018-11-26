2cce9f669c247c781c77f2ae3233b894adee1740
IHCoding <s.imal@yahoo.com>
2018-11-26 11:02:37 +0100

    Add project files.

---
+++ Personkartotek/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Personkartotek.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
